a052ae33cb15cd33d2970d9818fa12c95da83c40
Sebastian <sebastianmibanez@gmail.com>
2026-04-30 14:00:35 -0400

    update logo to miraza2.0

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom'
-import logoImg from '../assets/miraza-kintsugi.png'
+import logoImg from '../assets/miraza2.0.png'
 
 function Footer() {
   const year = new Date().getFullYear()
--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import { Link, useLocation } from 'react-router-dom'
-import logoImg from '../assets/miraza-kintsugi.png'
+import logoImg from '../assets/miraza2.0.png'
 import './Navbar.css'
 
 function Navbar() {
